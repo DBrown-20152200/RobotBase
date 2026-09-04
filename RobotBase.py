b87dd907affce29f1ca09be1b604fbe665482dae
Dylan Brown <20152200@tafe.wa.edu.au>
2026-09-04 11:51:30 +0800

    Motor component functional

--- RobotBase.py
+++ RobotBase.py
@@ -6,8 +6,8 @@ class RobotBase:
         
         Args:
             _name: Robot name (str)
-            _battery_level: Battery charge (float)
-            _is_moving: Is robot moving? (bool)
+            _battery: A robot HAS a battery
+            _motor: A robot HAS a motor
             _sensor_readings: Readings from sensor (dict)
         """
         self._name = name
@@ -23,42 +23,44 @@ class RobotBase:
     def move(self, distance):
         if self.speed < 0:
             Motor.move_forward(distance)
+            Battery.drain(15)            
         elif self.speed > 0:
             Motor.move_backward(distance)
+            Battery.drain(15)
         else:
             self.stop()
 
-    def get_sensor_reading(self, name):
-        """Get sensor value of specified robot
+    # def get_sensor_reading(self, name):
+    #     """Get sensor value of specified robot
         
-        Args:
-            name: Sensor name (str)
+    #     Args:
+    #         name: Sensor name (str)
         
-        Raises:
-            ValueError: If name doesn't match self._name
+    #     Raises:
+    #         ValueError: If name doesn't match self._name
 
-        Returns:
-            Measurement from the robot's sensors
-        """
-        if (name not in self._sensor_readings):
-            raise ValueError(f"Sensor - {name} - not found")
+    #     Returns:
+    #         Measurement from the robot's sensors
+    #     """
+    #     if (name not in self._sensor_readings):
+    #         raise ValueError(f"Sensor - {name} - not found")
         
-        return self._sensor_readings[name]
+    #     return self._sensor_readings[name]
 
-    def set_sensor_reading(self, name, value):
-        """Set sensor value of specified robot
+    # def set_sensor_reading(self, name, value):
+    #     """Set sensor value of specified robot
         
-        Args:
-            name: Sensor name (str)
-            value: Sensor value (float)
+    #     Args:
+    #         name: Sensor name (str)
+    #         value: Sensor value (float)
         
-        Raises:
-            ValueError: Value not float
-        """
-        if (value < 0):
-            raise ValueError("Please enter a number")
+    #     Raises:
+    #         ValueError: Value not float
+    #     """
+    #     if (value < 0):
+    #         raise ValueError("Please enter a number")
         
-        self._sensor_readings[name] = str(value) + "cm"
+    #     self._sensor_readings[name] = str(value) + "cm"
 
     def report_status(self):
         """Robot status summary
@@ -70,7 +72,9 @@ class RobotBase:
 
     def __str__(self):
         """User-friendly report of the robot's status"""
-        pass
+        return(f"Robot Name: {self.name} " +
+              f"Battery: {self._battery_level}" +
+              f"Motor: {self._motor}")
     def __repr__(self):
         """Dev-friendly report of all variables"""
         pass
@@ -97,28 +101,38 @@ class Battery:
         return (self.level/self.capacity) * 100
 
     def __str__(self):
-        return (f"Battery charge at {self.charge_percentage():.2f}%")
+        return (f"Battery charge at {self.charge_percentage():.2f}% ")
 
 class Motor:
-    def __init__(self, speed: int, is_running: bool):
-        self.speed = speed        
-        self.is_running = is_running
+    def __init__(self, speed: int):
+        self.speed = speed
+        if (speed != 0):
+            self.is_running = True
+        else:
+            self.is_running = False
 
     def move_forward(self, distance:int):
         """Moves the forward a set distance"""
         self.distance = distance
+        self.is_running = True
 
     def move_backward(self, distance:int):
         """Moves backward a set distance"""
         self.distance = 0 - distance
+        self.is_running = True
 
     def stop(self):
         """Stops movement"""
         self.distance = 0
+        self.is_running = False
 
     def set_speed(self, speed:int):
         """Set specific robot speed"""
         self.speed = speed
+
+    def __str__(self):
+        """Prints user friendly string"""
+        return (f"Motor speed: {self.speed} m/s")
 
 class Sensor:
     def __init__(self, sensor_type: str):
@@ -139,18 +153,18 @@ class Sensor:
 
 # Tests RobotBase class if this is the main file
 if __name__ == "__main__":
-    robot = RobotBase("Bingus", 180, False, {"Front":"24cm"})
-    android = RobotBase("Sbeve", 270, True, {"Front":"13cm"})
+    robot = RobotBase("Bingus", 180, 24, {"Front":"24cm"})
+    android = RobotBase("Sbeve", 270, 0, {"Front":"13cm"})
 
     print(robot)
 
     print(android)
 
-    print(RobotBase.report_status(robot))
-    print(RobotBase.report_status(android))
+    # print(RobotBase.report_status(robot))
+    # print(RobotBase.report_status(android))
 
-    RobotBase.set_sensor_reading(robot, "Rear", 47)
-    print(RobotBase.get_sensor_reading(robot, "Rear"))
+    # RobotBase.set_sensor_reading(robot, "Rear", 47)
+    # print(RobotBase.get_sensor_reading(robot, "Rear"))
 
-    print(RobotBase.report_status(robot))
+    # print(RobotBase.report_status(robot))
 
