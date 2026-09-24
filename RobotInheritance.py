e960e58577be6072917a48c0a357887326c10d0a
Dylan Brown <20152200@tafe.wa.edu.au>
2026-09-24 13:23:17 +0800

    Linefollower only moves when on black line

--- RobotInheritance.py
+++ RobotInheritance.py
@@ -8,16 +8,24 @@ class LineFollower(RobotBase):
         super().__init__(name, battery, motor, sensor)
 
 
-    def detect_obstacle(self):
+    def detect_line(self):
         if self._sensor.reading == "black_line":
             return False
         else:
             return True
 
+    def move(self, distance):
+        self.detect_line()
+        if self.detect_line() == True:
+            self._motor.stop()
+        else:
+            super().move(distance)
+
 if __name__ == "__main__":
     Steve = LineFollower("Steve", Battery(75, 75), Motor(0, False), Sensor("Colour Sensor"))
     print(Steve)
 
+    Steve._sensor.reading = "black_line"
     Steve.move(5)
     print(Steve)
 
